6287a7c2b5042bc4efcec9d71a6bdf063cbdaa82
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-09-01 18:52:11 +0000

    Added session user profile page

--- app.py
+++ app.py
@@ -66,6 +66,8 @@ def signup():
         # Put the new user into 'session' cookie
         session['user'] = request.form.get("username").lower()
         flash("Signup is successful, Welcome to FilmZone")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("signup.html")
 
 
@@ -84,6 +86,7 @@ def login():
               existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 # Invalid password match message to user
                 flash("Incorrect Username and/or Password")
@@ -96,6 +99,14 @@ def login():
 
     # Returns user to main login page to try again
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # Grabs the session user's username from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
