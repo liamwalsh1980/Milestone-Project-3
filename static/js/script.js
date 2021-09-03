cf90eca942264c13c6532a7b737398b5a5fb3c39
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-09-03 21:36:14 +0000

    Genre Selection added to add film template

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown();
     $('.collapsible').collapsible();
+    $('select').formSelect();
   });
