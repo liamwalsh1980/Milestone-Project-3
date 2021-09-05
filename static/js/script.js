7a689dbaa3bcc4c39988712a1dfa53926bd492c7
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-09-05 15:47:32 +0000

    Bug found and fixed subject to further tests

--- static/js/script.js
+++ static/js/script.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
         let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
         if ($("select.validate").prop("required")) {
-            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+            // Bug found: Originally code has "display" which caused a small bug - notes for reference on my readme
+            $("select.validate").css({ "display-none": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
         }
         $(".select-wrapper input.select-dropdown").on("focusin", function () {
             $(this).parent(".select-wrapper").on("change", function () {
